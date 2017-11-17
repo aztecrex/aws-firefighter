4a2f40ae3de0695151559e202c2f54cd9a8ab45f
Greg Wiley <gwiley@cj.com>
2017-11-17 09:20:50 -0800

    experiment progress

--- experiment/fightfire.sh
+++ experiment/fightfire.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-# env -S $(< temp.json jp 'Credentials | [join(`"="`, [`"AWS_ACCESS_KEY"`,AccessKeyId]),join(`"="`, [`"AWS_SECRET_ACCESS_KEY"`,SecretAccessKey]),join(`"="`, [`"AWS_SESSION_TOKEN"`,SessionToken])] | join(`"\n"`, @)')
+responsef=$(mktemp)
+
+aws lambda invoke --function-name firefighter "$responsef" > /dev/null
+
+cat "$responsef"
+
+estr=$(< "$responsef" jp 'Credentials | [join(`"="`, [`"AWS_ACCESS_KEY"`,AccessKeyId]),join(`"="`, [`"AWS_SECRET_ACCESS_KEY"`,SecretAccessKey]),join(`"="`, [`"AWS_SESSION_TOKEN"`,SessionToken])] | join(`"\n"`, @)')
+
+rm -f "$responsef"
+
+echo "$estr"
+
+# estr=$(< "$responsef" jp 'Credentials | [join(`"="`, [`"AWS_ACCESS_KEY"`,AccessKeyId]),join(`"="`, [`"AWS_SECRET_ACCESS_KEY"`,SecretAccessKey]),join(`"="`, [`"AWS_SESSION_TOKEN"`,SessionToken])] | join(`"\n"`, @)')
+
+# env -S $(< "$responsef" jp 'Credentials | [join(`"="`, [`"AWS_ACCESS_KEY"`,AccessKeyId]),join(`"="`, [`"AWS_SECRET_ACCESS_KEY"`,SecretAccessKey]),join(`"="`, [`"AWS_SESSION_TOKEN"`,SessionToken])] | join(`"\n"`, @)')
 
 
 
