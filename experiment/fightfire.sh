7a8cc06978176850d12ee670af9e327060d630cb
Greg Wiley <aztec.rex@jammm.com>
2017-11-25 17:06:19 -0800

    fight fire command from API url

--- experiment/fightfire.sh
+++ experiment/fightfire.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
-responsef=$(mktemp)
+fenv="$(dirname $0)/fightfire.env"
 
-prof="$1"
-shift
+if [ ! -x "$(which jp)" ]; then
+    echo "JMESPath processer 'jp' required" 1>&2
+    exit 2
+fi
 
-aws --profile "$prof" lambda invoke --function-name firefighter "$responsef" > /dev/null
+if [ -f "$fenv" ]; then
+    source "$fenv"
+fi
 
-estr=$(< "$responsef" jp -u 'Credentials | [join(`"="`, [`"AWS_ACCESS_KEY_ID"`,AccessKeyId]),join(`"="`, [`"AWS_SECRET_ACCESS_KEY"`,SecretAccessKey]),join(`"="`, [`"AWS_SESSION_TOKEN"`,SessionToken])] | join(`" "`, @)')
-rm -f "$responsef"
+if [ -z "$FIREFIGHTER_VENDOR" ]; then
+    echo "environment variable 'FIREFIGHTER_VENDOR' required" 1>&2
+    exit 3
+fi
+
+
+estr="$(    curl "${FIREFIGHTER_VENDOR}" \
+        | jp -u 'Credentials | [join(`"="`, [`"AWS_ACCESS_KEY_ID"`,AccessKeyId]),join(`"="`, [`"AWS_SECRET_ACCESS_KEY"`,SecretAccessKey]),join(`"="`, [`"AWS_SESSION_TOKEN"`,SessionToken])] | join(`" "`, @)' \
+    )"
 
 env -S "$estr" aws "${@}"
